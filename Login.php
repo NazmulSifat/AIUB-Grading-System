af1c6b5e56cb55ce0512216a574528cc00b0d52c
Nazmul Sifat <nazmulsifat1788@gmail.com>
2025-11-07 01:59:19 +0600

    my first

---
+++ Login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Aiub Protal</title>
+
+    <link rel="stylesheet" href="log.css">
+</head>
+
+<body>
+    <div class="apac">
+
+        <center>
+            <form>
+                <img src="aiublogo.png" style="width: 80px;height: 80px;">
+                <h1>American International University-Bangladesh</h1>
+                <p> Where leaders are created </p>
+
+                <p>Sign in with your organizational id number.</p>
+
+
+
+                <input type="text" placeholder="2x-5xxxx-x">
+                <br>
+                <input type="text" placeholder="Password">
+                <br>
+
+                <button type="Submit" class="btn"> Login </button>
+
+            </form>
+        </center>
+    </div>
+</body>
+
+
+
+</html>
